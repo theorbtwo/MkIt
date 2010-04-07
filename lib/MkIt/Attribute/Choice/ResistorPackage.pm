fcfbbba8cbf48059afd3d3b5d71c79d4252aae03
James Mastros <james@mastros.biz>
2010-04-07 23:05:40 +0100

    Add missing MkIt::A::C::ResistorPackage.pm

---
+++ lib/MkIt/Attribute/Choice/ResistorPackage.pm
@@ -0,0 +1,13 @@
+package MkIt::Attribute::Choice::ResistorPackage;
+use Moose;
+use Moose::Util::TypeConstraints;
+extends 'MkIt::Attribute::Choice';
+
+coerce __PACKAGE__,
+  'Str' => sub {__PACKAGE__->new($_)};
+
+sub allowed_values {
+  'through hole', '1206';
+}
+
+1;
